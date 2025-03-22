ab9f3312e0cd916aa0dce5f40bd1adac38ba9eb3
rrmoutonbt <info@rrmoutonbt.com>
2025-03-22 11:56:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,21 +2,16 @@
 set -e
 
 # Define repository URLs (use these if not already set as environment variables)
-BLOCKCHAIN_CORE_REPO=${BLOCKCHAIN_CORE_REPO:-"https://github.com/rrmoutonbt/Future-Exchange.git"}
-ISO_COMPLIANCE_REPO=${ISO_COMPLIANCE_REPO:-"https://github.com/rrmoutonbt/ISO-Complian-on-chain-enforcement-7-.git"}
-BLOCKCHAIN_NETWORK_REPO=${BLOCKCHAIN_NETWORK_REPO:-"https://github.com/rrmoutonbt/Network-Layer-ISO-Compliant-.git"}
+BLOCKCHAIN_CORE_REPO=${BLOCKCHAIN_CORE_REPO:-"https://github.com/rrmoutonbt/Future-Exchange-Blockchain
+.git"}
 
 # Clone repositories
 echo "Cloning repositories..."
-[ ! -d "Future-Exchange" ] && git clone $BLOCKCHAIN_CORE_REPO Future-Exchange
-[ ! -d "ISO-Complian-on-chain-enforcement-7-" ] && git clone $ISO_COMPLIANCE_REPO ISO-Complian-on-chain-enforcement-7-
-[ ! -d "Network-Layer-ISO-Compliant-" ] && git clone $BLOCKCHAIN_NETWORK_REPO Network-Layer-ISO-Compliant-
+[ ! -d "Future-Exchange" ] && git clone $BLOCKCHAIN_CORE_REPO Future-Exchange-Blockchain
 
 # Update repositories if they already exist
 echo "Updating repositories..."
-cd Future-Exchange && git pull && cd ..
-cd ISO-Complian-on-chain-enforcement-7- && git pull && cd ..
-cd Network-Layer-ISO-Compliant- && git pull && cd ..
+cd Future-Exchange-Blockchain && git pull && cd ..
 
 # Create build directory
 echo "Setting up build directory..."
@@ -25,6 +20,5 @@ cd build
 
 # Run CMake with proper paths
 echo "Running CMake..."
-cmake -DBLOCKCHAIN_CORE_DIR=../Future-Exchange \
-      -DISO_COMPLIANCE_DIR=../ISO-Complian-on-chain-enforcement-7- \
-      -DBLOCKCHAIN_NETWORK_DIR=../Network-Layer-ISO-Compliant- \
+cmake -DBLOCKCHAIN_CORE_DIR=../Future-Exchange-Blockchain \
+      
